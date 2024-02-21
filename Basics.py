639b3348875076242ca54a83e6ecc209a6fbb50e
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-02-21 19:38:02 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -8,5 +8,46 @@ student   #output-> sita
 
 # data type
 a=20
+type(a)  #int
+#boolean
+b='true'
+type(b)
+#float
+a=2.0
 type(a)
+ # complex ->real  & imeginary part
+a=3+4j  
+type(a)
+
+#operator.....> Arithmetic operator
+a=4
+b=3
+sum=a+b
+diff=a-b
+mul= a*b
+div=a/b
+mod=a%b
+print(sum)
+print(diff)
+print(mul)
+print(div)
+print(mod)
+
+#logical operator(&, |)
+a=True
+b=False
+print(a&a)
+print(a&b)
+print(a|b)
+print(a|a)
+
+#relatonal operator (<,>,==,!=)
+a=50
+b=80
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+
+
 
