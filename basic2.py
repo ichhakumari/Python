89a8d04ba644f6c70edbed82e175ffbaf8d85c13
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-02-24 21:02:44 +0530

    added functions.py

--- basic2.py
+++ basic2.py
@@ -29,3 +29,44 @@ for x in fruits:
   print(x)
   if x == "banana":
     break
+
+
+#function -> block of code which perform specfic task
+def hello():
+  print("hello python")
+
+hello()
+
+def odd_even(x):
+  if x%2==0:
+    print(x, "is even")
+  else:
+    print(x,"is odd")
+
+odd_even(9)
+
+#annourmous function - lambda function
+g=lambda x: x*x*x
+
+g(5)
+
+# lambda with filter
+#filter odd numbers
+l1=[34,21,6,54,7,9,53]
+
+final_list=list(filter(lambda x: (x%2!=0),l1))
+
+final_list
+
+
+#lambda with map   -> multiply all element with 2
+l1=[1,2,3,4,5,6]
+final_l1=list(map(lambda x: x*2,l1))
+
+final_l1
+
+#lambda with reduce function -> use to call a final sum or value
+from functools import reduce
+sum=reduce(lambda x,y: x+y,l1)
+
+sum
