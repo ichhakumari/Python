c94a3e34f8d81980922ef4fd971de5295aeee92e
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-02-26 20:52:51 +0530

    numpy.py

--- oops.py
+++ oops.py
@@ -145,3 +145,17 @@ g1.assign_string_three("child one")
 print(g1.show_naam())
 print(g1.show_str_one())
 print(g1.show_str_three())
+
+
+#Numpy-> Numerical Python ->computation
+# use array and multi dimenstion array
+import numpy as np
+n1=np.array([10,20,30,44])  # single array
+print(n1)
+
+#multi dimentional array
+n2=np.array([[11,22,33,44],[43,53,63,73],[66,77,88,99]])
+print(n2)
+
+print(type(n1))
+print(type(n2))
