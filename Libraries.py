d6136abdbfeeb7c1311003b1fe2a11d058ceef71
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-02-27 20:03:39 +0530

    Update Libraries.py

--- Libraries.py
+++ Libraries.py
@@ -1,1 +1,14 @@
+#Numpy-> Numerical Python ->computation
+# use array and multi dimenstion array
+import numpy as np
+n1=np.array([10,20,30,44])  # single array
+print(n1)
+
+#multi dimentional array
+n2=np.array([[11,22,33,44],[43,53,63,73],[66,77,88,99]])
+print(n2)
+
+print(type(n1))
+print(type(n2))
+
 
