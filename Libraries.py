ffc542953350e1b588b1ebaf8170e80f97fbb8e2
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-02-29 22:03:11 +0530

    Update Libraries.py

--- Libraries.py
+++ Libraries.py
@@ -98,4 +98,17 @@ print(np.sum([n1,n2],axis=0))
 #only row->axis=1
 print(np.sum([n1,n2],axis=1))
 
+#basic addition
+n1=n1+1
+print(n1)
+
+n2=n2-2
+print(n2)
+
+n1=n1*2
+print(n1)
+
+n1=n1/2
+print(n1)
+
 
