6db768b86036c5adc42e02aba64b8aeef0d7fe50
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-08-11 14:43:32 +0530

    Update love_calculator.py

--- love_calculator.py
+++ love_calculator.py
@@ -1,1 +1,12 @@
-
+def love_calculator():
+    n1= input("enter your name: ")
+    n2= input("enter your partner name: ")
+    c= n1 + n2
+    
+    true = c.count('t')+c.count('r')+c.count('u')+c.count('e')
+    love= c.count('l')+c.count('0')+c.count('v')+c.count('e')
+    
+    score= str(true)+ str(love)
+    print(score)
+    
+love_calculator()    
